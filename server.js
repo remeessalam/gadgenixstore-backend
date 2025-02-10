88b0049af20e759f627875f817fb79cc45d430ad
remeessalam <remeesreme4u@gmail.com>
2025-02-10 15:06:24 +0530

    cors added new link

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://gadgenixstore-backend.vercel.app/",
     credentials: true,
   })
 );
